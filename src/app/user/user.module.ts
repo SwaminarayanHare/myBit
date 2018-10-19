8c524cde90f22ec33689f2ece785b388d66a150d
sagartalaviya91 <sagartalaviya91@gmail.com>
2018-10-19 12:10:22 +0530

    chart change

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,8 +4,10 @@ import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UpdatedocumentComponent } from './components/updatedocument/updatedocument.component';
 import { UserhomeComponent } from './components/userhome/userhome.component';
+import { UserchartsComponent } from './components/usercharts/usercharts.component';
 import { CommoncomponentModule } from '../commoncomponent/commoncomponent.module';
 import { FormsModule }   from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 import {  SubscriptionService, AssetService, StockService,PricingService } from '../_services';
 
 @NgModule({
@@ -13,9 +15,10 @@ import {  SubscriptionService, AssetService, StockService,PricingService } from 
     CommonModule,
     CommoncomponentModule,
     UserRoutingModule,
-    FormsModule
+    FormsModule,
+    ChartsModule
   ],
-  declarations: [ DashboardComponent, UserhomeComponent, UpdatedocumentComponent ],
+  declarations: [ DashboardComponent, UserhomeComponent, UpdatedocumentComponent, UserchartsComponent ],
   providers:[ SubscriptionService, AssetService , StockService, PricingService]
 })
 export class UserModule { }
